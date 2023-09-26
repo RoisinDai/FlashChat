4274c7f57a354e9c37a8e70edcf0aa3d1f66a1f7
Roisin Dai <roisin@Roisins-MacBook-Air.local>
2023-09-26 00:43:21 -0400

    updated client and server side code

--- app.js
+++ app.js
@@ -1,22 +1,108 @@
-/*
-    active chatting server application
-*/
 var app = require('express')()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
+
+const rooms = {};
 
 server.listen(3000, () => {
     console.log("server activated successfully")
 })
 
-// express can process statis resources
-// make public list into static resources list
 app.use(require('express').static('public'))
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-        console.log('New user has connected')
-});
+const avatarImages = [
+    'avatar1.png',
+    'avatar2.png',
+    'avatar3.png',
+    'avatar4.png',
+    'avatar5.png'
+];
+
+function getRandomAvatarImage() {
+    const randomIndex = Math.floor(Math.random() * avatarImages.length);
+    return avatarImages[randomIndex];
+}
+
+io.sockets.on('connection', (socket) => {
+    console.log('New user has connected')
+    socket.on('create room', (roomCode, username, maxNum, avatarUrl) => {
+        rooms[roomCode] = {
+            generatedCode: roomCode, 
+            users: [],
+            currNum: 1,
+            maxNum: maxNum,
+        }
+        rooms[roomCode].users.push({username, avatarUrl});
+
+        socket.join(roomCode);
+        console.log(socket.id + " has created the room, username = " + username + " avatar: " + avatarUrl + " with maxNum = " + maxNum + " code: " + roomCode);
+        console.log(rooms);
+        console.table(rooms[roomCode].users);
+        const dataToEmits = {
+            rooms,
+            roomCode: roomCode
+          };
+        io.emit('userList', dataToEmits);
+    });    
+    
+    socket.on('join room', (enteredCode, username, avatarUrl) => {
+        // Check if the room exists
+        if (rooms[enteredCode]) {
+            if(rooms[enteredCode].currNum != rooms[enteredCode].maxNum) {
+                rooms[enteredCode].users.push({username, avatarUrl});
+            rooms[enteredCode].currNum++;
+            socket.join(enteredCode);
+            console.log(socket.id + " has joined the room, username = " + username + " avatar: " + avatarUrl);
+            console.log(rooms);
+            console.table(rooms[enteredCode].users);
+        } else {
+                socket.emit('rooms full');
+                return;
+            } 
+        } else {
+            socket.emit('roomNotFound'); 
+            return;
+        }
+        const dataToEmit = {
+            rooms,
+            roomCode: enteredCode
+          };
+        io.emit('userList', dataToEmit);
+
+        const newUserData = {
+            username: username,
+            avatarUrl: avatarUrl,
+            roomCode: enteredCode
+        }
+
+        io.emit('addUser', newUserData);
+        socket.username = username;
+        socket.avatar = avatarUrl;
+        socket.enteredCode = enteredCode;
+    });
+
+    socket.on('disconnect', () => {
+        // delete username and avatarUrl from users
+        // update userList
+        let idx = rooms[socket.enteredCode].users.findIndex(item => item.username === socket.username);
+        rooms[socket.enteredCode].users.splice(idx, 1);
+        io.emit('delUser', {
+            username: socket.username,
+            avatar: socket.avatarUrl
+        });
+        const dataToEmit = {
+            rooms: rooms,
+            roomCode: socket.enteredCode
+          };
+        io.emit('userList', dataToEmit)
+    })
+
+    socket.on('sendMessage', data => {
+        console.log(data)
+        io.emit('receiveMessage', data)
+        })
+    });
--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,205 @@
-/*
-    Main Chatroom Functions
-*/
+var socket = io('http://localhost:3000');
 
-/*
-    1. Connect socket.io service
-*/
+var mainBox = document.getElementById('mainBox');
+var createRoomBtn = document.getElementById('createRoomBtn')
+var createRoomBox = document.getElementById('createRoomBox');
+var backButton1 = document.getElementById('backButton1');
+var joinRoomBox = document.getElementById('joinRoomBox');
+var joinRoomBtn = document.getElementById('joinRoomBtn');
+var backButton2 = document.getElementById('backButton2');
+var startRoomBtn = document.getElementById('startRoomBtn');
+var chatBox = document.getElementById('chatBox');
+var enterRoomBtn = document.getElementById('enterRoomBtn');
+var generatedCode = document.getElementById('generatedCode');
+var username = document.getElementById('username');
+var selfUsername = document.getElementById('selfUsername');
+var maxPeople = document.getElementById('maxPeople');
+var enteredCode = document.getElementById('enteredCode');
+var currentNumber = document.getElementById('currentNumber');
+var userCount = document.getElementById("userCount");
+var selfAvatar = document.getElementById('selfAvatar');
 
-var socket = io('http://localhost:3000')
-
-document.getElementById('createRoomBtn').addEventListener('click', function() {
-    createRoomFunction();
+createRoomBtn.addEventListener('click', function() {
+    event.preventDefault();
+    generatedCode.textContent = codeGeneration();
+    console.log(username.value);
+    if (!username.value) {
+        alert('Please enter a display name');
+        return;
+    }
+    mainBox.style.display = "none";
+    createRoomBox.style.display = "block";
 });
 
-function createRoomFunction() {
-    alert("Create Room button clicked!");
-    // You can add your logic here
+backButton1.addEventListener('click', function() {
+    event.preventDefault();
+    createRoomBox.style.display = "none";
+    mainBox.style.display = "block  ";
+});
+
+joinRoomBtn.addEventListener('click', function() {
+    event.preventDefault();
+    if (!username.value) {
+        alert('Please enter a display name');
+        return;
+    }
+    mainBox.style.display = "none";
+    joinRoomBox.style.display = "block";
+});
+
+backButton2.addEventListener('click', function() {
+    event.preventDefault();
+    joinRoomBox.style.display = "none";
+    mainBox.style.display = "block";
+});
+
+startRoomBtn.addEventListener('click', function() {
+    event.preventDefault();
+    createRoomBox.style.display = "none";
+    chatBox.style.display = "block";
+    var image = getRandomAvatarImage();
+    selfAvatar.src = image;
+    socket.emit('create room', generatedCode, username.value, maxPeople.value, image)
+    selfUsername.textContent = username.value;
+});
+
+enterRoomBtn.addEventListener('click', function() {
+    joinRoomBox.style.display = "none";
+    chatBox.style.display = "block";
+    if (!enteredCode.value) {
+        alert('Please enter a room code');
+        return;
+    }
+    var image = getRandomAvatarImage();
+    selfAvatar.src = image;
+    socket.emit('join room', enteredCode.value, username.value, image);
+    selfUsername.textContent = username.value;
+});
+
+function codeGeneration() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+
+    for (let i = 0; i < 8; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters.charAt(randomIndex);
+    }
+    generatedCode = code;
+    return code;
 }
+
+const avatarImages = [
+    'avatar1.png',
+    'avatar2.png',
+    'avatar3.png',
+    'avatar4.png',
+    'avatar5.png'
+];
+
+function getRandomAvatarImage() {
+    const randomIndex = Math.floor(Math.random() * avatarImages.length);
+    return avatarImages[randomIndex];
+}
+
+socket.on('roomNotFound', () => {
+    joinRoomBox.style.display = "block";
+    chatBox.style.display = "none";
+    alert('No rooms found. Please enter the correct room code.');
+});
+
+socket.on('rooms full', () => {
+    joinRoomBox.style.display = "block";
+    chatBox.style.display = "none";
+    alert('Room has reached maximum capacity.');
+});
+
+
+socket.on('userList', data => {
+    // alert(data.rooms[data.roomCode].currNum);
+    $('.user-list ul').html('');
+    data.rooms[data.roomCode].users.forEach(item => {
+        $('.user-list ul').append(`
+            <li class="user">
+                <div class="avatar"><img src ="${item.avatarUrl}" alt= "" /></div>            
+                <div class="username">${item.username}</div>
+            </li>
+        `);
+    });
+    userCount.textContent = "ChatRoom (" + data.rooms[data.roomCode].currNum + ")";
+
+});
+
+socket.on('addUser', data => {
+    // send a system message
+    $('.box-bd').append(`
+        <div class="system">
+            <p class="message_system">
+                <span class="content">${data.username} has joined the chat</span>
+            </p>
+        </div>
+    `);
+});
+
+socket.on('delUser', data=> {
+    // send a system message
+    $('.box-bd').append(`
+    <div class="system">
+        <p class="message_system">
+            <span class="content">${data.username} has left the chat</span>
+        </p>
+    </div>
+`);
+});
+
+// chat features
+
+$('.btn-send').on('click', () =>{
+    // obtain chat content
+    var content = $('#content').val().trim();
+    $('#content').val('');
+    if(!content) return alert ('Please enter content');
+
+    // send data to server
+    socket.emit('sendMessage', {
+        msg: content,
+        selfUsername: username.value,
+        selfAvatar: selfAvatar.src
+    });
+});
+
+
+socket.on('receiveMessage', data => {
+    if(data.selfUsername === username.value) {
+        // my messages
+        $('.box-bd').append(`
+        <div class="message-box">
+        <div class="my-message">
+            <img class="avatar" src="${data.selfAvatar}" alt=""/>
+            <div class="content">
+                <div class="bubble">
+                    <div class="bubble_cont">${data.msg}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+        `)
+    } else {
+        // others' messages
+        $('.box-bd').append(`
+        <div class="message-box">
+            <div class="other-message">
+                <img class="avatar" src="${data.selfAvatar}" alt=""/>
+                    <div class="content">
+                        <div class="nickname">${data.selfUsername}</div>
+                        <div class="bubble">
+                            <div class="bubble_cont">${data.msg}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `)
+
+    }
+});
 
