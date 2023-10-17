ecc8ffd975d18bbafa72f725117dbf6fc8e0d00b
Roisin Dai <roisin@v1040-wn-rt-a-127-221.campus-dynamic.uwaterloo.ca>
2023-10-16 20:40:07 -0400

    made updates

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { setServers } = require('dns');
+
 var app = require('express')()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
@@ -106,3 +108,63 @@ io.sockets.on('connection', (socket) => {
         io.emit('receiveMessage', data)
         })
     });
+
+    
+
+    // socket.on('join room', (enteredCode, username, avatarUrl) => {
+    //     // Check if the room exists
+    //     if (rooms[enteredCode]) {
+    //         if(rooms[enteredCode].currNum != rooms[enteredCode].maxNum) {
+    //             rooms[enteredCode].users.push({username, avatarUrl});
+    //         rooms[enteredCode].currNum++;
+    //         socket.join(enteredCode);
+    //         console.log(socket.id + " has joined the room, username = " + username + " avatar: " + avatarUrl);
+    //         console.log(rooms);
+    //         console.table(rooms[enteredCode].users);
+    //     } else {
+    //             socket.emit('rooms full');
+    //             return;
+    //         } 
+    //     } else {
+    //         socket.emit('roomNotFound'); 
+    //         return;
+    //     }
+    //     const dataToEmit = {
+    //         rooms,
+    //         roomCode: enteredCode
+    //       };
+    //     io.emit('userList', dataToEmit);
+
+    //     const newUserData = {
+    //         username: username,
+    //         avatarUrl: avatarUrl,
+    //         roomCode: enteredCode
+    //     }
+
+    //     io.emit('addUser', newUserData);
+    //     socket.username = username;
+    //     socket.avatar = avatarUrl;
+    //     socket.enteredCode = enteredCode;
+    // });
+
+    // socket.on('disconnect', () => {
+    //     // delete username and avatarUrl from users
+    //     // update userList
+    //     let idx = rooms[socket.enteredCode].users.findIndex(item => item.username === socket.username);
+    //     rooms[socket.enteredCode].users.splice(idx, 1);
+    //     io.emit('delUser', {
+    //         username: socket.username,
+    //         avatar: socket.avatarUrl
+    //     });
+    //     const dataToEmit = {
+    //         rooms: rooms,
+    //         roomCode: socket.enteredCode
+    //       };
+    //     io.emit('userList', dataToEmit)
+    // })
+
+    // socket.on('sendMessage', data => {
+    //     console.log(data)
+    //     io.emit('receiveMessage', data)
+    //     })
+    // });
